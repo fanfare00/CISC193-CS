3e8702f25ffdfc0dc5ce13560e9f5cb732db738d
unknown <james-mccarthy@att.net>
2015-02-04 11:41:33 -0800

    inital quirky programming commit

---
+++ TA#1.Q - Quirky Programming/JamesQuirks/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace JamesQuirks
+{
+    partial class quirkyForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelWelcome = new System.Windows.Forms.Label();
+            this.labelDateTime = new System.Windows.Forms.Label();
+            this.buttonElapsedTime = new System.Windows.Forms.Button();
+            this.buttonJamesQuirk1 = new System.Windows.Forms.Button();
+            this.buttonJamesQuirk2 = new System.Windows.Forms.Button();
+            this.buttonQuentinQuirk1 = new System.Windows.Forms.Button();
+            this.buttonQuentinQuirk2 = new System.Windows.Forms.Button();
+            this.buttonXQuirk1 = new System.Windows.Forms.Button();
+            this.buttonXQurik2 = new System.Windows.Forms.Button();
+            this.buttonYQuirk1 = new System.Windows.Forms.Button();
+            this.buttonYQuirk2 = new System.Windows.Forms.Button();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.labelQuirkHeader = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelWelcome
+            // 
+            this.labelWelcome.AutoSize = true;
+            this.labelWelcome.Location = new System.Drawing.Point(256, 44);
+            this.labelWelcome.Name = "labelWelcome";
+            this.labelWelcome.Size = new System.Drawing.Size(196, 13);
+            this.labelWelcome.TabIndex = 0;
+            this.labelWelcome.Text = "Welcome to James\' GUI Quirky Program";
+            // 
+            // labelDateTime
+            // 
+            this.labelDateTime.AutoSize = true;
+            this.labelDateTime.Location = new System.Drawing.Point(689, 523);
+            this.labelDateTime.Name = "labelDateTime";
+            this.labelDateTime.Size = new System.Drawing.Size(83, 13);
+            this.labelDateTime.TabIndex = 1;
+            this.labelDateTime.Text = "11:26 2/4/2015";
+            // 
+            // buttonElapsedTime
+            // 
+            this.buttonElapsedTime.Location = new System.Drawing.Point(12, 526);
+            this.buttonElapsedTime.Name = "buttonElapsedTime";
+            this.buttonElapsedTime.Size = new System.Drawing.Size(100, 23);
+            this.buttonElapsedTime.TabIndex = 2;
+            this.buttonElapsedTime.Text = "Elapsed Time";
+            this.buttonElapsedTime.UseVisualStyleBackColor = true;
+            // 
+            // buttonJamesQuirk1
+            // 
+            this.buttonJamesQuirk1.Location = new System.Drawing.Point(26, 145);
+            this.buttonJamesQuirk1.Name = "buttonJamesQuirk1";
+            this.buttonJamesQuirk1.Size = new System.Drawing.Size(106, 23);
+            this.buttonJamesQuirk1.TabIndex = 3;
+            this.buttonJamesQuirk1.Text = "James\' Quirk #1";
+            this.buttonJamesQuirk1.UseVisualStyleBackColor = true;
+            // 
+            // buttonJamesQuirk2
+            // 
+            this.buttonJamesQuirk2.Location = new System.Drawing.Point(26, 174);
+            this.buttonJamesQuirk2.Name = "buttonJamesQuirk2";
+            this.buttonJamesQuirk2.Size = new System.Drawing.Size(106, 23);
+            this.buttonJamesQuirk2.TabIndex = 4;
+            this.buttonJamesQuirk2.Text = "James\' Quirk #2";
+            this.buttonJamesQuirk2.UseVisualStyleBackColor = true;
+            // 
+            // buttonQuentinQuirk1
+            // 
+            this.buttonQuentinQuirk1.Location = new System.Drawing.Point(26, 307);
+            this.buttonQuentinQuirk1.Name = "buttonQuentinQuirk1";
+            this.buttonQuentinQuirk1.Size = new System.Drawing.Size(106, 23);
+            this.buttonQuentinQuirk1.TabIndex = 5;
+            this.buttonQuentinQuirk1.Text = "button1";
+            this.buttonQuentinQuirk1.UseVisualStyleBackColor = true;
+            // 
+            // buttonQuentinQuirk2
+            // 
+            this.buttonQuentinQuirk2.Location = new System.Drawing.Point(26, 336);
+            this.buttonQuentinQuirk2.Name = "buttonQuentinQuirk2";
+            this.buttonQuentinQuirk2.Size = new System.Drawing.Size(106, 23);
+            this.buttonQuentinQuirk2.TabIndex = 6;
+            this.buttonQuentinQuirk2.Text = "button1";
+            this.buttonQuentinQuirk2.UseVisualStyleBackColor = true;
+            // 
+            // buttonXQuirk1
+            // 
+            this.buttonXQuirk1.Location = new System.Drawing.Point(692, 145);
+            this.buttonXQuirk1.Name = "buttonXQuirk1";
+            this.buttonXQuirk1.Size = new System.Drawing.Size(75, 23);
+            this.buttonXQuirk1.TabIndex = 7;
+            this.buttonXQuirk1.Text = "button1";
+            this.buttonXQuirk1.UseVisualStyleBackColor = true;
+            // 
+            // buttonXQurik2
+            // 
+            this.buttonXQurik2.Location = new System.Drawing.Point(692, 185);
+            this.buttonXQurik2.Name = "buttonXQurik2";
+            this.buttonXQurik2.Size = new System.Drawing.Size(75, 23);
+            this.buttonXQurik2.TabIndex = 8;
+            this.buttonXQurik2.Text = "button1";
+            this.buttonXQurik2.UseVisualStyleBackColor = true;
+            // 
+            // buttonYQuirk1
+            // 
+            this.buttonYQuirk1.Location = new System.Drawing.Point(692, 307);
+            this.buttonYQuirk1.Name = "buttonYQuirk1";
+            this.buttonYQuirk1.Size = new System.Drawing.Size(75, 23);
+            this.buttonYQuirk1.TabIndex = 9;
+            this.buttonYQuirk1.Text = "button1";
+            this.buttonYQuirk1.UseVisualStyleBackColor = true;
+            // 
+            // buttonYQuirk2
+            // 
+            this.buttonYQuirk2.Location = new System.Drawing.Point(692, 336);
+            this.buttonYQuirk2.Name = "buttonYQuirk2";
+            this.buttonYQuirk2.Size = new System.Drawing.Size(75, 23);
+            this.buttonYQuirk2.TabIndex = 10;
+            this.buttonYQuirk2.Text = "button1";
+            this.buttonYQuirk2.UseVisualStyleBackColor = true;
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Location = new System.Drawing.Point(358, 462);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(75, 23);
+            this.buttonExit.TabIndex = 11;
+            this.buttonExit.Text = "Exit";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            // 
+            // labelQuirkHeader
+            // 
+            this.labelQuirkHeader.AutoSize = true;
+            this.labelQuirkHeader.Location = new System.Drawing.Point(315, 145);
+            this.labelQuirkHeader.Name = "labelQuirkHeader";
+            this.labelQuirkHeader.Size = new System.Drawing.Size(108, 13);
+            this.labelQuirkHeader.TabIndex = 12;
+            this.labelQuirkHeader.Text = "So-and-So\'s Quirk #1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(156, 336);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(452, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "really big and really long and really long and really big quirky text that is rea" +
+    "lly big and really long";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(259, 174);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(233, 126);
+            this.pictureBox1.TabIndex = 14;
+            this.pictureBox1.TabStop = false;
+            // 
+            // quirkyForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelQuirkHeader);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.buttonYQuirk2);
+            this.Controls.Add(this.buttonYQuirk1);
+            this.Controls.Add(this.buttonXQurik2);
+            this.Controls.Add(this.buttonXQuirk1);
+            this.Controls.Add(this.buttonQuentinQuirk2);
+            this.Controls.Add(this.buttonQuentinQuirk1);
+            this.Controls.Add(this.buttonJamesQuirk2);
+            this.Controls.Add(this.buttonJamesQuirk1);
+            this.Controls.Add(this.buttonElapsedTime);
+            this.Controls.Add(this.labelDateTime);
+            this.Controls.Add(this.labelWelcome);
+            this.Name = "quirkyForm";
+            this.Text = "James\' GUI Quirky Program";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelWelcome;
+        private System.Windows.Forms.Label labelDateTime;
+        private System.Windows.Forms.Button buttonElapsedTime;
+        private System.Windows.Forms.Button buttonJamesQuirk1;
+        private System.Windows.Forms.Button buttonJamesQuirk2;
+        private System.Windows.Forms.Button buttonQuentinQuirk1;
+        private System.Windows.Forms.Button buttonQuentinQuirk2;
+        private System.Windows.Forms.Button buttonXQuirk1;
+        private System.Windows.Forms.Button buttonXQurik2;
+        private System.Windows.Forms.Button buttonYQuirk1;
+        private System.Windows.Forms.Button buttonYQuirk2;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.Label labelQuirkHeader;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
